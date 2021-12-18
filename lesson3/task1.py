71ea7e3a1874b687960ef13d6b56b2feee58a82c
nikuha <yoginika@yandex.ru>
2021-12-18 15:18:28 +0300

    lesson3

--- lesson3/task1.py
+++ lesson3/task1.py
@@ -17,7 +17,6 @@ def dict_hash(dictionary):
 client = MongoClient('127.0.0.1', 27017)
 db = client['hh']
 vacancies_collection = db.vacancies
-vacancies_collection.delete_many({})
 
 main_url = 'https://hh.ru/search/vacancy'
 params = {'text': 'Python', 'page': 1, 'items_on_page': 20}
